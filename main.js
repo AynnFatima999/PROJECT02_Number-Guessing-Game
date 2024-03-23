49f9ccc00023871df0f7f44e2876d35b0e38e6da
45-Assiagnments <aaleali999@gmail.com>
2024-03-23 17:52:31 +0500

    initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+#!  /usr/bin/env node
+import inquirer from "inquirer";
+let randomNumber = Math.floor(Math.random() * 10 + 1);
+let user = await inquirer.prompt([{
+        name: "userGuess",
+        type: "number",
+        message: "Guess the Number between 1 to 10:"
+    }]);
+if (user.userGuess === randomNumber) {
+    console.log("Congratulations! You guess right.");
+}
+else {
+    console.log("Aww! you guessed wrong. Try again.");
+}
